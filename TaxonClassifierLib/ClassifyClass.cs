1eaeae4ee6de0b925126c1aef25b5ca6a8b5d812
Brian <Brian@DESKTOP-69IN6P3>
2025-05-23 09:28:02 +0200

    Add project files.

---
+++ TaxonClassifierLib/ClassifyClass.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TaxonClassifierLib
+{
+    public class ClassifyClass
+    {
+        public static string special_characters = "ẅëẗÿüïöäḧẍẃéŕýúíóṕǻáśǵḱĺǽǿźćǘńḿẁèỳùìòàùǹåæøẽỹũĩõãṽñŵêŷûîôâŝĝĥĵẑĉþœßŋħĸłµçð";
+
+        public static JObject classify(string text, JObject taxonomy, JObject settings)
+        {
+            JObject result = new JObject();
+            JObject classes = new JObject();
+
+            return result;
+        }
+    }
+}
