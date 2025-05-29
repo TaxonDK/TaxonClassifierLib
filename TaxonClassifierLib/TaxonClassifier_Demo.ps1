e98ea8a71209e2d3694ec2290e5e2d238bd70464
Brian <Brian@taxon.dk>
2025-05-29 11:16:08 +0200

    Updated demo Powershell script

--- TaxonClassifierLib/TaxonClassifier_Demo.ps1
+++ TaxonClassifierLib/TaxonClassifier_Demo.ps1
@@ -1,7 +1,7 @@
 # dllFile needs to be the full path to the .dll
 $dllFile = "C:\Users\Brian\source\repos\TaxonClassifierLib\TaxonClassifierLib\bin\Debug\net8.0\TaxonClassifierLib.dll"
-$os2kleFile = "C:\Users\Brian\Projects\Open_Source_TaxonClassifier\OS2KLE.json"
-$os2kle_lookupFile = "C:\Users\Brian\Projects\Open_Source_TaxonClassifier\OS2KLE_lookup.json"
+$taxonomyFile = "C:\Users\Brian\Projects\Open_Source_TaxonClassifier\OS2KLE.json"
+$taxonomy_lookupFile = "C:\Users\Brian\Projects\Open_Source_TaxonClassifier\OS2KLE_lookup.json"
 
 $text = "hul hul hul vej vej rotte"
 
@@ -12,35 +12,35 @@ $types = $taxonClassifier.GetTypes()
 $classify = $types[0].GetMethod("classifyText")
 $makeLookup = $types[1].GetMethod("makeLookupTaxonomy")
 
-if(Test-Path -Path $os2kle_lookupFile)
+if(Test-Path -Path $taxonomy_lookupFile)
 {
-	Write-Output "Using lookup taxonomy $os2kle_lookupFile"
+	Write-Output "Using lookup taxonomy $taxonomy_lookupFile"
 
-	$os2kle_lookupJSON = Get-Content $os2kle_lookupFile -Raw
+	$taxonomy_lookupJSON = Get-Content $taxonomy_lookupFile -Raw
 }
 else
 {
 	Write-Output "Making and saving the lookup taxonomy"
 
-	if(-not(Test-Path -Path $os2kleFile))
+	if(-not(Test-Path -Path $taxonomyFile))
 	{
 		Write-Output "Downloading OS2KLE from Github"
 		
 		$url = "https://github.com/os2kle/os2kle/raw/refs/heads/master/OS2KLE.json"
 		
-		Invoke-WebRequest -Uri $url -OutFile $os2kleFile	
+		Invoke-WebRequest -Uri $url -OutFile $File
 	}
 
-	$os2kleJSON = Get-Content $os2kleFile -Raw
+	$taxonomyJSON = Get-Content $taxonomyFile -Raw
 
-	$os2kle_lookupJSON = $makeLookup.Invoke($null, @($os2kleJSON.ToString()))
+	$taxonomy_lookupJSON = $makeLookup.Invoke($null, @($taxonomyJSON.ToString()))
 
-	Set-Content -Path $os2kle_lookupFile -Value $os2kle_lookupJSON
+	Set-Content -Path $taxonomy_lookupFile -Value $taxonomy_lookupJSON
 }
 
 $settings = [System.Collections.Generic.Dictionary[string,string]]::new()
 
-$result = $classify.Invoke($null, @($text, $os2kle_lookupJSON.ToString(), $settings))
+$result = $classify.Invoke($null, @($text, $taxonomy_lookupJSON.ToString(), $settings))
 
 Write-Output $result.ToString()
 
